6ead761f29943a3955dcd8efb3869a1d952f3613
Atif Aslam <aslam.renu.soft43@gmail.com>
2018-10-16 14:19:50 -0800

    Update completed

---
+++ Serial_Booking/src/main/java/com/renu/s_b/SerialBookingApplication.java
@@ -0,0 +1,12 @@
+package com.renu.s_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerialBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerialBookingApplication.class, args);
+	}
+}
---
+++ Serial_Booking/src/test/java/com/renu/s_b/SerialBookingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.renu.s_b;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SerialBookingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
